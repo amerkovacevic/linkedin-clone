642d8650c9feb2920ba0a9ba22081ad5a06ab8f8
Amer Kovacevic <79953503+amerkovacevic@users.noreply.github.com>
2021-05-10 01:28:15 -0500

    Login page and home page made, no linkage yet.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import Login from './components/Login'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div>
-      here
+    <div classname='App'>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path='/home'>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
- 
+   
